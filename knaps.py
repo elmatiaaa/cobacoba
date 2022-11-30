27adb9b930d50b1f5674fb87a6618bb85525449a
elmatiaaa <112562792+elmatiaaa@users.noreply.github.com>
2022-12-01 02:29:33 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -13,6 +13,10 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
 
+# Split Data
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=1)
+
 st.write(""" 
 # Cek data
 """)
