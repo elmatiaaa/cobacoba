aaa900bc446eb607ce6b5b78758edf8458428f83
elmatiaaa <112562792+elmatiaaa@users.noreply.github.com>
2022-12-01 02:43:17 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -35,7 +35,7 @@ with tab2:
     
 
 with tab3:
-   X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
     from sklearn.preprocessing import StandardScaler
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
