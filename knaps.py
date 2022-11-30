beed8b6f88bf956594ae46fc9c35016c1c878673
elmatiaaa <112562792+elmatiaaa@users.noreply.github.com>
2022-12-01 02:22:22 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -23,18 +23,18 @@ tab1, tab2, tab3, tab4 = st.tabs(["Import Data", "Preprocessing", "Modelling", "
 with dataframe:
     st.write('Data Jamur')
     dataset,data= st.tabs(['Dataset',"data"])
-    with dataset:
-        st.dataframe(df)
-    with tab1:
-        st.write("Import Data")
-        wine = pd.read_csv("https://raw.githubusercontent.com/elmatiaaa/Machine-Learning/main/winequality-red.csv")
-        st.dataframe(wine)
+with dataset:
+    st.dataframe(df)
+with tab1:
+    st.write("Import Data")
+    wine = pd.read_csv("https://raw.githubusercontent.com/elmatiaaa/Machine-Learning/main/winequality-red.csv")
+    st.dataframe(wine)
 
-    with tab2:
-        quality_series = wine.loc[:, "quality"]
-        quality_categorical_series = pd.cut(quality_series, [0, 5, 10], labels=["bad", "good"])
-        wine["quality"] = quality_categorical_series
-        wine
+with tab2:
+    quality_series = wine.loc[:, "quality"]
+    quality_categorical_series = pd.cut(quality_series, [0, 5, 10], labels=["bad", "good"])
+    wine["quality"] = quality_categorical_series
+    wine
     
 
 with modeling:
